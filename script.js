e83cc46829f295a99a356841b592c5d28f36054d
Govind-Sudarsan18 <govindsudarsan@gmail.com>
2026-03-03 14:48:08 +0530

    Update and rename 3️⃣ script.js to script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-// Smooth scrolling
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
+    anchor.addEventListener('click', function(e) {
         e.preventDefault();
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
